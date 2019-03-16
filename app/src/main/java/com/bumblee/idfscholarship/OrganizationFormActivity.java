537654ac3861c20cb3038a12aa6f0c568c47883f
Raj Bohra <rajbohra10@gmail.com>
2019-03-16 13:53:22 +0530

    get json data from form

--- app/src/main/java/com/bumblee/idfscholarship/OrganizationFormActivity.java
+++ app/src/main/java/com/bumblee/idfscholarship/OrganizationFormActivity.java
@@ -1,13 +1,68 @@
 package com.bumblee.idfscholarship;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class OrganizationFormActivity extends AppCompatActivity {
+
+    private EditText etName, etUserName, etDesc, etNumber, etLink;
+    private Button submitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organization_form);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        etName = findViewById(R.id.etName);
+        etUserName = findViewById(R.id.etUserName);
+        etDesc = findViewById(R.id.etDescription);
+        etNumber = findViewById(R.id.etPhone);
+        etLink = findViewById(R.id.etWebsite);
+        submitBtn = findViewById(R.id.submitBtn);
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = etName.getText().toString().trim();
+                String username = etUserName.getText().toString().trim();
+                String desc = etDesc.getText().toString().trim();
+                String phoneNumber = etNumber.getText().toString().trim();
+                String websiteLink = etLink.getText().toString().trim();
+
+                JSONObject orgDetails = new JSONObject();
+                try {
+                    orgDetails.put("username", username);
+                    orgDetails.put("name", name);
+                    orgDetails.put("description", desc);
+                    orgDetails.put("phoneNumber", phoneNumber);
+                    orgDetails.put("website", websiteLink);
+
+                    Log.d("JSON-DATA", orgDetails.toString());
+
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
     }
+
+
 }
--- app/src/main/java/com/bumblee/idfscholarship/RegisterAs.java
+++ app/src/main/java/com/bumblee/idfscholarship/RegisterAs.java
@@ -3,8 +3,12 @@ package com.bumblee.idfscholarship;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterAs extends AppCompatActivity {
 
@@ -25,8 +29,30 @@ public class RegisterAs extends AppCompatActivity {
                 Intent intent = new Intent(RegisterAs.this, OrganizationFormActivity.class);
 //                intent.putExtra("name", getIntent().getExtras().getString("userName"));
                 startActivity(intent);
-                finish();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.signOut:
+                signOutUser();
+                break;
+        }
+        return true;
+    }
+
+    public void signOutUser(){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(RegisterAs.this, LoginActivity.class));
+        finish();
     }
 }
